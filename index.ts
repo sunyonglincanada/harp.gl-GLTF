baa78411bccba42a4f4d4e62d705e3c8b6282204
Eric Sun <eric.sun@trader.ca>
2021-01-30 01:34:29 -0500

    Add test glb file and update latitude & longtitude.

--- index.ts
+++ index.ts
@@ -5,7 +5,15 @@
  */
 
 import { GeoCoordinates, TileKey } from "@here/harp-geoutils";
-import { MapAnchor, MapViewEventNames, RenderEvent, Tile, TileObject, registerTileObject, DataSource } from "@here/harp-mapview";
+import {
+  MapAnchor,
+  MapViewEventNames,
+  RenderEvent,
+  Tile,
+  TileObject,
+  registerTileObject,
+  DataSource,
+} from "@here/harp-mapview";
 import * as THREE from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -39,8 +47,6 @@ mapView.lookAt({
 });
 
 const clock = new THREE.Clock();
-
-
 
 let figure: MapAnchor<THREE.Group> | undefined;
 let mixer: THREE.AnimationMixer | undefined;
@@ -90,80 +96,80 @@ mapView.beginAnimation();
 
 // GLTF START
 const loaderNew = new GLTFLoader();
-const tile = new Tile(this, tileKey: TileKey); //Or whatever Tile implementation you have.
+const tile = new Tile(this, tileKey); //Or whatever Tile implementation you have.
 
 // Assuming you know where to put the model....
-const geoPoint = new GeoCoordinates(latitude, longitude, altitude);
+const geoPoint = new GeoCoordinates(40.70497091, -74.0135);
 const tangentSpace = {
-    position: new THREE.Vector3(),
-    xAxis: new THREE.Vector3(),
-    yAxis: new THREE.Vector3(),
-    zAxis: new THREE.Vector3()
+  position: new THREE.Vector3(),
+  xAxis: new THREE.Vector3(),
+  yAxis: new THREE.Vector3(),
+  zAxis: new THREE.Vector3(),
 };
 this.projection.localTangentSpace(geoPoint, tangentSpace);
 const worldPos = tangentSpace.position;
 const scaleFactor = this.projection.getScaleFactor(worldPos);
 worldPos.sub(tile.center);
-let data : ArrayBuffer = 'resources/H22.glb'
+let data: ArrayBuffer = "resources/H22.glb";
 loaderNew.parse(
-               data,
-                ".",
-                (gltf: GLTF) => {
-                    for (const child of gltf.scene.children) {
-                        const obj: TileObject = child;
-                        obj.displacement = worldPos;
-                        obj.scale.setScalar(scaleFactor);
-                        obj.setRotationFromMatrix(
-                            new THREE.Matrix4().makeBasis(
-                                tangentSpace.xAxis,
-                                tangentSpace.yAxis,
-                                tangentSpace.zAxis
-                            )
-                        );
-                        obj.renderOrder = this.m_renderOrder ?? 1000;
-                        obj.receiveShadow = tile.mapView.shadowsEnabled;
-                        obj.castShadow = tile.mapView.shadowsEnabled;
-                        if ((obj as any).material !== undefined) {
-                            const alphaTest = 0.66;
-                            const map = (obj as any).material.map;
-                            const material = new THREE.MeshPhongMaterial({
-                                map,
-                                shininess: 60,
-                                alphaTest,
-                                polygonOffset: true, // prevent z-fighting at ground level
-                                polygonOffsetFactor: -1.0,
-                                polygonOffsetUnits: -1.0
-                            });
-                            (obj as any).material = material;
+  data,
+  ".",
+  (gltf: GLTF) => {
+    for (const child of gltf.scene.children) {
+      const obj: TileObject = child;
+      obj.displacement = worldPos;
+      obj.scale.setScalar(scaleFactor);
+      obj.setRotationFromMatrix(
+        new THREE.Matrix4().makeBasis(
+          tangentSpace.xAxis,
+          tangentSpace.yAxis,
+          tangentSpace.zAxis
+        )
+      );
+      obj.renderOrder = this.m_renderOrder ?? 1000;
+      obj.receiveShadow = tile.mapView.shadowsEnabled;
+      obj.castShadow = tile.mapView.shadowsEnabled;
+      if ((obj as any).material !== undefined) {
+        const alphaTest = 0.66;
+        const map = (obj as any).material.map;
+        const material = new THREE.MeshPhongMaterial({
+          map,
+          shininess: 60,
+          alphaTest,
+          polygonOffset: true, // prevent z-fighting at ground level
+          polygonOffsetFactor: -1.0,
+          polygonOffsetUnits: -1.0,
+        });
+        (obj as any).material = material;
 
-                            //Needed for shadows... can remove if you don't have shadows
-                            const customDepthMaterial = new THREE.MeshDepthMaterial({
-                                depthPacking: THREE.RGBADepthPacking,
-                                map,
-                                alphaTest
-                            });
+        //Needed for shadows... can remove if you don't have shadows
+        const customDepthMaterial = new THREE.MeshDepthMaterial({
+          depthPacking: THREE.RGBADepthPacking,
+          map,
+          alphaTest,
+        });
 
-                            (obj as any).customDepthMaterial = customDepthMaterial;
-                        }
-                        registerTileObject(tile, obj, GeometryKind.Building);
-                        if (obj.type === "Mesh") {
-                            const mesh = obj as THREE.Mesh;
-                            mesh.geometry.computeBoundingBox();
-                            this.maxGeometryHeight = Math.max(
-                                this.maxGeometryHeight,
-                                mesh.geometry.boundingBox!.max.z
-                            );
-                        }
-                        tile.objects.push(obj);
-                    }
-                    Tile.requestUpdate();
-                    return await Promise.resolve();
-                },
-                (error: ErrorEvent) => {
-                    logger.error(`Error parsing GLTF model`);
-                    return await Promise.resolve();
-                }
-            );
+        (obj as any).customDepthMaterial = customDepthMaterial;
+      }
+      registerTileObject(tile, obj, GeometryKind.Building);
+      if (obj.type === "Mesh") {
+        const mesh = obj as THREE.Mesh;
+        mesh.geometry.computeBoundingBox();
+        this.maxGeometryHeight = Math.max(
+          this.maxGeometryHeight,
+          mesh.geometry.boundingBox!.max.z
+        );
+      }
+      tile.objects.push(obj);
+    }
+    Tile.requestUpdate();
+    return await Promise.resolve();
+  },
+  (error: ErrorEvent) => {
+    logger.error(`Error parsing GLTF model`);
+    return await Promise.resolve();
+  }
+);
 
 // GLTF END
 
