94ffe6731ea2a1c43a99a8fa38dea923e9fc2ee2
Eric Sun <sunyonglincanada@gmail.com>
2021-02-04 23:38:20 -0500

    Add geojson points example.

---
+++ config.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2019-2021 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @hidden */
+export const apikey = "J0IJdYzKDYS3nHVDDEWETIqK3nAcxqW42vz7xeSq61M";
+
+/** @hidden */
+export const copyrightInfo = [
+    {
+        id: "here.com",
+        year: new Date().getFullYear(),
+        label: "HERE",
+        link: "https://legal.here.com/terms"
+    }
+];
